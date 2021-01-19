9bb5bc129e6304f4aac648d32d284b0605d82a35
TikDeveloper <tiko0000.80.tm@gmail.com>
2021-01-19 12:34:59 +0400

    First push

---
+++ js/home.js
@@ -0,0 +1,43 @@
+function burgerClick(x) {
+    x.classList.toggle('change')
+    document.querySelector('.header__navMobile').classList.toggle('header__navMobileActive')
+}
+function fadeSectionTop(){
+    document.querySelector('.section__topChildCenter').classList.add('fadeSectionTop')
+}
+function hideLoader(){
+    document.querySelector('.loader__main').classList.add('loader__hide')
+}
+function enableScroll() {
+    document.querySelector('body').style.overflow = 'initial'
+}
+
+window.addEventListener('load',()=>{
+
+    let x = false;
+    window.addEventListener('scroll',e => {
+            const cardsMainOffset = document.querySelector('.feature__cardMain').offsetHeight;
+            let scrollVal = window.pageYOffset;
+            if(scrollVal - cardsMainOffset > 150 && !x){
+                const cards = document.querySelectorAll('.feature__card');
+                for(let i = 0; i < cards.length ; i++){
+                    setTimeout(function(){
+                        cards[i].classList.add('fadeFeatures')
+                    },500*i)
+                }
+                x = true;
+            }
+    })
+    
+    setTimeout(function(){
+        fadeSectionTop()
+    },500)
+
+
+    setTimeout(function(){
+        hideLoader()
+        enableScroll()
+    },1000)
+
+
+})
---
+++ js/pricing.js
@@ -0,0 +1,17 @@
+function hideLoader(){
+    document.querySelector('.loader__main').classList.add('loader__hide')
+}
+function enableScroll() {
+    document.querySelector('body').style.overflow = 'initial'
+}
+window.addEventListener('load',()=>{
+
+
+
+    setTimeout(function(){
+        hideLoader()
+        enableScroll()
+    },1000)
+
+
+})
